6a2cc293cf28ba5f8553fb8bebdb71a32c72cf0f
KeenCat <hmkim8011@naver.com>
2016-07-08 16:52:15 +0900

    I want to go home

--- wr_freq/wr_freq.py
+++ wr_freq/wr_freq.py
@@ -4,6 +4,7 @@ from os.path import basename
 import os
 import csv
 import re
+import chardet
 import glob
 from konlpy.tag import Kkma
 from konlpy.corpus import kolaw
@@ -26,6 +27,52 @@ def WriteDictToCSV(csv_file,csv_columns,dict_data):
     return
 
 
+def GET_DATASET(doc):
+    dic = []
+    tmppos = []
+    pos = Hannanum().pos(doc)
+    tagged_words = Kkma().pos(doc)
+    for word in pos:
+        if word[1] == 'N':
+            tmppos.append(word)
+
+    tmp_pos = tmppos
+
+    for words in tmp_pos:
+        for stop in stopwords:
+            if unicode(words[0]) == unicode(stop):
+                tmppos = remove_values_from_list(tmppos, words)
+
+    cnt = Counter(tmppos)
+    i = 1
+    for freq_word in cnt.most_common(50):
+        dic.append({'구분': 'unigram', '순위': str(i), '단어': freq_word[0][0], '횟수': str(freq_word[1])})
+        i = i + 1
+
+    words = [w for w, t in tagged_words]
+    ignored_words = stopwords
+    finder = collocations.BigramCollocationFinder.from_words(words)
+    finder.apply_word_filter(lambda w: len(w) < 2 or w in ignored_words)
+    finder.apply_freq_filter(int(dic[19]['횟수']))  # only bigrams that appear 3+ times
+
+    for bigram in (finder.nbest(measures.pmi, 20)):
+        dic.append({'구분': 'bigram', '순위': '1~20', '단어': unicode(bigram[0]) + unicode(bigram[1]),
+                    '횟수': str(dic[19]['횟수']) + u"회 이상"})
+
+    finder = collocations.TrigramCollocationFinder.from_words(words)
+    finder.apply_word_filter(lambda w: len(w) < 2 or w in ignored_words)
+    finder.apply_freq_filter(int(dic[19]['횟수']))  # only trigrams that appear 3+ times
+
+    for trigram in (finder.nbest(measures.pmi, 20)):
+        dic.append(
+            {'구분': 'trigram', '순위': '1~20', '단어': unicode(trigram[0]) + unicode(trigram[1]) + unicode(trigram[2]),
+             '횟수': str(dic[19]['횟수']) + u"회 이상"})
+
+    csv_columns = ['구분', '순위', '단어', '횟수']
+
+    csv_file = filename.split('.')[0] + "freq.csv"
+
+    WriteDictToCSV(csv_file, csv_columns, dic)
 
 stopwords = []
 stoptext = open("stopwords.txt", "r")
@@ -43,57 +90,29 @@ measures2 = collocations.TrigramAssocMeasures()
 def remove_values_from_list(the_list, val):
    return [value for value in the_list if value != val]
 
-pwd = os.getcwd()+'\\txt'
+pwd = os.getcwd()+'\\document'
 
 
 for path, dirs, files in os.walk(pwd):
     for file in files:
         filename = file.decode('cp949')
         if (filename.split('.')[1] == 'txt'):
-            dic = []
-            tmppos = []
-            doc = unicode(open(("txt/"+filename)).read(), 'utf-8')
-            pos = Hannanum().pos(doc)
-            tagged_words = Kkma().pos(doc)
-            for word in pos:
-                if word[1] == 'N':
-                    tmppos.append(word)
+            doc = open(("document/"+filename)).read()
+            doc_type = chardet.detect(doc)
 
-            tmp_pos = tmppos
-
-            for words in tmp_pos:
-                for stop in stopwords:
-                    if unicode(words[0]) == unicode(stop):
-                        tmppos = remove_values_from_list(tmppos, words)
-
-
-            cnt = Counter(tmppos)
-            i = 1
-            for freq_word in cnt.most_common(50):
-                dic.append({'구분': 'unigram', '순위': str(i), '단어': freq_word[0][0], '횟수': str(freq_word[1])})
-                i=i+1
-
-            words = [w for w, t in tagged_words]
-            ignored_words = stopwords
-            finder = collocations.BigramCollocationFinder.from_words(words)
-            finder.apply_word_filter(lambda w: len(w) < 2 or w in ignored_words)
-            finder.apply_freq_filter(int(dic[19]['횟수'])) # only bigrams that appear 3+ times
-
-            for bigram in (finder.nbest(measures.pmi, 20)):
-                dic.append({'구분': 'bigram','순위': '1~20', '단어':unicode(bigram[0])+unicode(bigram[1]), '횟수':str(dic[19]['횟수']) + u"회 이상"})
-
-            finder = collocations.TrigramCollocationFinder.from_words(words)
-            finder.apply_word_filter(lambda w: len(w) < 2 or w in ignored_words)
-            finder.apply_freq_filter(int(dic[19]['횟수'])) # only trigrams that appear 3+ times
+            if(doc_type['encoding'] == 'utf-8'):
+                doc = unicode(doc, 'utf-8')
+                GET_DATASET(doc)
+            elif(doc_type['encoding'] == 'UTF-16LE'):
+                doc = doc.decode('utf-16').encode('utf-8')
+                doc = unicode(doc, 'utf-8')
+                GET_DATASET(doc)
+        elif(filename.split('.')[1] == 'csv'):
+            d = ""
+            i=0
+            for row in csv.reader(open("document/"+filename)):
+                d= d + (unicode(row[2], 'utf-8'))
+            GET_DATASET(d)
 
 
-            for trigram in (finder.nbest(measures.pmi, 20)):
-                dic.append({'구분': 'trigram', '순위': '1~20', '단어':unicode(trigram[0])+unicode(trigram[1])+unicode(trigram[2]), '횟수':str(dic[19]['횟수']) + u"회 이상"})
 
-            csv_columns = ['구분', '순위', '단어', '횟수']
-
-            csv_file = filename.split('.')[0] + "freq.csv"
-
-            WriteDictToCSV(csv_file, csv_columns, dic)
-        else:
-            print(u"text파일이 존재하지 않습니다.")
