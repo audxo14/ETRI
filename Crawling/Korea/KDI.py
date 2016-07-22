cf2cb6fcd73694b41c84ba9c94f78dae80de1a88
KeenCat <hmkim8011@naver.com>
2016-07-22 09:15:33 +0900

    Add comment

--- Crawling/Korea/KDI.py
+++ Crawling/Korea/KDI.py
@@ -17,13 +17,14 @@ def get_html_KDI(pagenum):
     return soup
 
 def get_data_KDI(html):
-    keepgo = 1
-    tmp_conts = html.find_all(class_="rpt_conts")[0]
+    keepgo = 1 # determine if searching is continued.
+    tmp_conts = html.find_all(class_="rpt_conts")[0] 
     tmp_date = tmp_conts.find_all(class_="nlt")
     tmp_sup = tmp_conts.find_all(class_="rpt_sup")
     tmp_lst = tmp_conts.find_all(class_="rpt_lst")
     tmp_a = tmp_conts.find_all('a')
 
+    # Do it per article.
     for i in range(0, len(tmp_date), 2):
 
         month = str(tmp_date[i]).split('.')[1]
@@ -31,7 +32,8 @@ def get_data_KDI(html):
         day = str(day).split('</')[0]
 
         j = (i / 2)
-
+        # The data of date appears every 2 <a>
+        # If the date of article equals the date of yesterday, get the data of article.
         if month == currentmonth:
             if day == currentday:
 
@@ -49,12 +51,14 @@ def get_data_KDI(html):
                 tmp_link = "=HYPERLINK(\"http://www.kdi.re.kr/policy/" + tmp_str + "\")"
 
                 dataset.append({'구분': '중앙부처', '발표처': tmp_ori, '제목': tmp_title, '웹주소': tmp_link})
-
+            
+            # If not, stop to search.
             else:
                 keepgo = 0
         else:
             keepgo = 0
-
+        
+        # If there are today's articles, ignore that data but keep search.
         today = str(datetime.datetime.now()).split('-')[2]
         today = today.split(' ')[0]
         if month == str(datetime.datetime.now()).split('-')[1]:
@@ -74,6 +78,7 @@ def WriteDictToCSV(csv_file,csv_columns,dict_data):
             print("I/O error({0}): {1}".format(errno, strerror))
     return
 
+# If today is Monday, get the Friday's date. If not, get the date of yesterday.
 dataset = []
 if(datetime.datetime.now().weekday() == 0 ):
     date = str(datetime.datetime.now() - datetime.timedelta(days = 3)).split(" ")[0]
@@ -83,13 +88,15 @@ else:
 currentmonth = date.split('-')[1]
 currentday = date.split('-')[2]
 
-p = 1
+# Start first page.
+p = 1 # Page number
 keepgo = 1
 while (keepgo == 1):
     test_html = get_html_KDI(p)
     keepgo = get_data_KDI(test_html)
     p = p + 1
 
+# Sort by '발표처'
 dataset = sorted(dataset, key =itemgetter('발표처'))
 csv_columns = ['구분', '발표처', '제목', '웹주소']
 current_dir = os.path.dirname(os.path.abspath(__file__))    #Get current Path
